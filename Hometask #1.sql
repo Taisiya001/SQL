1048498fedc972ed9551c118f1d73f7ec796186e
Taisiya001 <93278469+Taisiya001@users.noreply.github.com>
2023-07-05 16:23:43 +0300

    Update Hometask #1.sql

--- Hometask #1.sql
+++ Hometask #1.sql
@@ -1,2 +1,3 @@
-SELECT * FROM telephone.new_table
+SELECT * FROM telephone.new_table;INSERT INTO `telephone`.`new_table` (`id`, `product_name`, `manufacture`, `product_count`, `price`) VALUES ('5', 'P20 Pro', 'Huawei', '5', '36000')
+  
 WHERE count > 2 AND model like 'Samsung';
