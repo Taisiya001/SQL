45b19729f9cc39d965ed8abafa242ae8b1d39cfc
Taisiya001 <93278469+Taisiya001@users.noreply.github.com>
2023-07-05 16:32:31 +0300

    Update Hometask #1.sql

--- Hometask #1.sql
+++ Hometask #1.sql
@@ -1,3 +1,14 @@
+CREATE TABLE `telephone`.`new_table1` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `product_name` VARCHAR(45) NOT NULL,
+  `manufacture` VARCHAR(45) NOT NULL,
+  `product_count` VARCHAR(45) NOT NULL,
+  `price` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
+
 SELECT * FROM telephone.new_table;INSERT INTO `telephone`.`new_table` (`id`, `product_name`, `manufacture`, `product_count`, `price`) VALUES ('5', 'P20 Pro', 'Huawei', '5', '36000')
   
-WHERE count > 2 AND model like 'Samsung';
+WHERE count > 2 AND model = 'Samsung';
+WHERE product_name like 'iPhone';
+WHERE product_name like 'Samsung';
+WHERE price like '8';
